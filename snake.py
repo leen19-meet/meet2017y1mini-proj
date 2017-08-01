c767ebf518fa88de70384c85c87d92113d7280e9
leen19-meet <leen19@meet.mit.edu>
2017-08-01 16:30:27 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -34,24 +34,82 @@ for i in range(START_LENGTH):
     stamp_list.append(stamp_ID)
     
     
-UP_ARROW = "Up" ​ #Make sure you pay attention to upper and lower
+UP_ARROW = "Up"  #Make sure you pay attention to upper and lower
 #case
-LEFT_ARROW = "Left" ​ #Pay attention to upper and lower case
-DOWN_ARROW = "Down" ​ #Pay attention to upper and lower case
-RIGHT_ARROW = "Right" ​ #Pay attention to upper and lower case
-TIME_STEP = 100 ​ #Update snake position after this many
+LEFT_ARROW = "Left"  #Pay attention to upper and lower case
+DOWN_ARROW = "Down"  #Pay attention to upper and lower case
+RIGHT_ARROW = "Right"  #Pay attention to upper and lower case
+TIME_STEP = 100  #Update snake position after this many
 #milliseconds
-SPACEBAR = "space" ​ # Careful, it's not supposed to be
-capitalized!
-UP = 0
+SPACEBAR = "space"  # Careful, it's not supposed to be
+#capitalized!
 #1. Make variables LEFT, DOWN, and RIGHT with values 1, 2, and 3
 ####WRITE YOUR CODE HERE!!
+UP=0
 DOWN=1
 LEFT=2
 RIGHT=3
-direction=up
+direction=UP
 
 def up():
     global direction
     direction= UP
+    move_snake()
+    print("you pressed the up key")
+
+def down():
+    global direction
+    direction= DOWN
+    move_snake()
+    print("you pressed the down key")
+
+def left():
+    global direction
+    direction= LEFT
+    move_snake()
+    print("you pressed the left key")
+
+def right():
+    global direction
+    direction= RIGHT
+    move_snake()
+    print("you pressed the right key")
     
+turtle.onkeypress(up, UP_ARROW)
+turtle.onkeypress(down, DOWN_ARROW)
+turtle.onkeypress(left,LEFT_ARROW)
+turtle.onkeypress(right,RIGHT_ARROW)
+turtle.listen()
+
+def move_snake():
+    my_pos= snake.pos()
+    x_pos= my_pos[0]
+    y_pos= my_pos[1]
+    if direction==RIGHT:
+        snake.goto(x_pos + SQUARE_SIZE, y_pos)
+        print("you moved right!")
+    elif direction==LEFT:
+        snake.goto(x_pos - SQUARE_SIZE, y_pos)
+        print("you moved left!")
+    elif direction==UP:
+        snake.goto(x_pos, y_pos + SQUARE_SIZE)
+        print("you moved up!")
+    elif direction==DOWN:
+        snake.goto(x_pos, y_pos - SQUARE_SIZE)
+        print("you moved down!")
+
+#4. Write the conditions for UP and DOWN on your own
+##### YOUR CODE HERE
+#Stamp new element and append new stamp in list
+#Remember: The snake position changed - update my_pos()
+    my_pos=snake.pos()
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last
+    #piece of the tail
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+
